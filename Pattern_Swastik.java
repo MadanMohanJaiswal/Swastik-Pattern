852791893d9479ddc84ab5f7eb6286413d110ade
MadanMohanJaiswal <105949837+MadanMohanJaiswal@users.noreply.github.com>
2022-05-21 21:44:05 +0530

    Add files via upload

---
+++ Pattern_Swastik.java
@@ -0,0 +1,23 @@
+// SWASTIK PATTERN.
+public class Pattern_Swastik {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//java.util.Scanner sc=new java.util.Scanner(System.in);
+		int n=9;
+		int mid=(n+1)/2;
+		for(int i=1 ; i<=n ; i++) {
+			for(int j=1 ; j<=n ; j++) {
+				if( j==mid || i==mid || (i==1 && j>mid) || (i<= mid && j==1) || (i == n && j<=mid) || (i>= mid && j==n))
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+		
+		
+
+	}
+
+}
